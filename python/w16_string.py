8c1004564df2da9257f68bc64b6b12b10aa05921
jonalf <jonalf@gmail.com>
2026-03-11 14:02:11 -0400

    +f strings

--- python/w16_string.py
+++ python/w16_string.py
@@ -5,9 +5,9 @@ def upcase(c):
         return chr(new_c)
     else:
         return c
-print(upcase('q'))
-print(upcase('C'))
-print(upcase('3'))
+print(f' q: {upcase('q')}')
+print(f' C: {upcase('C')}')
+print(f' 3: {upcase('3')}')
 
 
 def upstring(s):
@@ -21,5 +21,5 @@ def upstring(s):
         c+= 1
     return new_s
 
-print(upstring('hello'))
-print(upstring("What's up?"))
+print(f' hello: {upstring('hello')}')
+print(f" What's up: {upstring("What's up?")}")
