972a386b4087b9eda55f0254e86858921f6d427b
JonAlf Dyrland-Weaver <dw@stuy.edu>
2026-03-11 14:25:08 -0400

    string formatting issue

--- python/w16_string.py
+++ python/w16_string.py
@@ -5,21 +5,16 @@ def upcase(c):
         return chr(new_c)
     else:
         return c
-print(f' q: {upcase('q')}')
-print(f' C: {upcase('C')}')
-print(f' 3: {upcase('3')}')
+
 
 
 def upstring(s):
     new_s = ''
     c = 0
     while c < len(s):
-        if s[c] >= 'a' and s[c] <= 'z':
-            new_s+= upcase(s[c])
-        else:
-            new_s+= s[c]
+        new_s+= upcase(s[c])
         c+= 1
     return new_s
 
-print(f' hello: {upstring('hello')}')
-print(f" What's up: {upstring("What's up?")}")
+
+
