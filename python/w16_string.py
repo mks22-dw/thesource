041da601a252bf2dbb66a0ec2e29ad33a6a05fbe
jonalf <jonalf@gmail.com>
2026-03-11 14:29:16 -0400

    meger fix

--- python/w16_string.py
+++ python/w16_string.py
@@ -5,13 +5,9 @@ def upcase(c):
         return chr(new_c)
     else:
         return c
-<<<<<<< HEAD
 print(f" q: {upcase('q')}")
 print(f" C: {upcase('C')}")
 print(f" 3: {upcase('3')}")
-=======
-
->>>>>>> 972a386b4087b9eda55f0254e86858921f6d427b
 
 
 def upstring(s):
@@ -21,6 +17,3 @@ def upstring(s):
         new_s+= upcase(s[c])
         c+= 1
     return new_s
-
-
-
